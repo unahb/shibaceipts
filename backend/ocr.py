a316f8d26b80f57d382055cee32078f74950ff2a
Bhanu Garg <garg.bhanu1@gmail.com>
2021-10-23 00:23:51 -0400

    ocr parsing

--- backend/ocr.py
+++ backend/ocr.py
@@ -15,9 +15,9 @@ textract = boto3.client('textract')
 # Call Amazon Textract
 response = textract.detect_document_text(Document={'Bytes': imageBytes})
 
-#print(response)
+# print(response)
 
 # Print detected text
 for item in response["Blocks"]:
     if item["BlockType"] == "LINE":
-        print ('\033[94m' +  item["Text"] + '\033[0m')
+        print('\033[94m' + item["Text"] + '\033[0m')
