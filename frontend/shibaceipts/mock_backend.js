0a0b46d9cf0cf46a1523c9769c1229bfc62c0670
Twinov <hoeltgemarc@gmail.com>
2021-10-23 04:42:00 -0400

    dumb dabbing dog to the bottom

--- frontend/shibaceipts/mock_backend.js
+++ frontend/shibaceipts/mock_backend.js
@@ -1,18 +1,6 @@
 export const MockPosts = [
   {
     user: {
-      username: 'DogeEnthusiast',
-      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
-    },
-    shibaceipt: {
-      location: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
-      owner: 'DogeEnthusiast',
-      minter: 'ElonMustFan26',
-      value: '16,000D',
-    },
-  },
-  {
-    user: {
       username: 'ElonMuskFan26',
       avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340259093209088/super-fabulous-shiba-prints.png?width=631&height=631',
     },
@@ -35,10 +23,22 @@ export const MockPosts = [
       value: '1000D',
     },
   },
+  {
+    user: {
+      username: 'DogeEnthusiast',
+      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
+    },
+    shibaceipt: {
+      location: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
+      owner: 'DogeEnthusiast',
+      minter: 'ElonMustFan26',
+      value: '16,000D',
+    },
+  },
 ]
 
 export const MockCurrentUser = {
   username: 'ShibaInuLover31',
   avatar: 'https://media.discordapp.net/attachments/892561854503735346/895573192637296650/20150605_47a9ea.png?width=631&height=631',
-  shibaceipts_count: 21
+  shibaceipts_count: 21,
 }
