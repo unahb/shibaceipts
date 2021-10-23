5a41923a2922df70f7f117e4a843b5b93e221bfc
Twinov <hoeltgemarc@gmail.com>
2021-10-23 00:50:14 -0400

    switch to native elements

--- frontend/shibaceipts/screens/Home.js
+++ frontend/shibaceipts/screens/Home.js
@@ -1,71 +1,24 @@
 import React from 'react'
 import { FlatList } from 'react-native'
-import { NativeBaseProvider, Thumbnail, ListItem } from 'native-base'
+import { Card, Button, Icon } from 'react-native-elements'
 import {MockPosts} from '../mock_backend'
 
 const renderItem = (item) => {
   return (
-      <p>{JSON.stringify(item)}</p>
+    <Card>
+      <Card.Title>{item.user.username}</Card.Title> <Card.Divider />{' '}
+      <Card.Image source={{uri: item.shibaceipt}}>
+      </Card.Image>
+    </Card>
   )
 }
 
 export default function Home() {
-  console.log(MockPosts)
   return (
-    <NativeBaseProvider>
       <FlatList
         data={MockPosts}
         keyExtractor={(item) => item.shibaceipt}
         renderItem={({ item }) => renderItem(item)}
       />
-    </NativeBaseProvider>
-  )
-  return (
-    <NativeBaseProvider>
-      <View style={{ justifyContent: 'flex-start' }}>
-        <FlatList
-          data={MockPosts}
-          renderItem={({ item }) => (
-            <Card>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={item.user.avatar} />
-                  <Body>
-                    <Text>Kofi</Text>
-                    <Text note>26 Feb 2018</Text>
-                  </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image source={item.shibaceipt} style={{ height: 200, width: null, flex: 1 }} />
-              </CardItem>
-              <CardItem style={{ height: 45 }}>
-                <Left>
-                  <Button transparent>
-                    <Icon name='ios-heart-outline' style={{ color: 'black' }} />
-                  </Button>
-                  <Button transparent>
-                    <Icon name='ios-chatbubbles-outline' style={{ color: 'black' }} />
-                  </Button>
-                  <Button transparent>
-                    <Icon name='ios-send-outline' style={{ color: 'black' }} />
-                  </Button>
-                </Left>
-              </CardItem>
-              <CardItem style={{ height: 20 }}>
-                <Text>lol</Text>
-              </CardItem>
-              <CardItem>
-                <Body>
-                  <Text>
-                    <Text style={{ fontWeight: '800' }}>kofi </Text>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores soluta dolor eveniet fugiat rem ullam laudantium, quod accusamus autem numquam maxime tempora nisi commodi unde. Nisi repudiandae culpa omnis doloremque!
-                  </Text>
-                </Body>
-              </CardItem>
-            </Card>
-          )}
-        />
-      </View>
-    </NativeBaseProvider>
   )
 }
