c23d635df23daaae176706d6b1b65f373a19277d
Twinov <hoeltgemarc@gmail.com>
2021-10-23 17:14:32 -0400

    better looking details

--- frontend/shibaceipts/screens/ViewShibaceipt.js
+++ frontend/shibaceipts/screens/ViewShibaceipt.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import { Image, Text } from 'react-native-elements'
+import { Image, Text, Card, Button, Icon } from 'react-native-elements'
+import Countdown from 'react-countdown'
 
 export default function ViewShibaceipt({ route }) {
   return (
     <View style={{ alignItems: 'center' }}>
       <Image source={{ uri: route.params.location }} style={{ width: 300, height: 300 }} />
+      <Card>
+        {' '}
+        <Card.Title style={{ fontSize: '30px' }}>Price: {route.params.value}Ð</Card.Title> <Card.Divider />
+        <Text style={{ fontSize: 15 }}>
+          ⏳ Time Remaining: <Countdown date={route.params.expiration} />
+        </Text>
+        <Button icon={<Icon style={{ marginRight: 5 }} name='payments' color='#ffffff' />} buttonStyle={{ borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0, marginTop: 5 }} title='BUY NOW' />
+      </Card>
       <Text style={styles.Text}>Owner: {route.params.owner}</Text>
-      <Text style={styles.Text}>Minter: {route.params.minter}</Text>
-      <Text style={styles.Text}>Value: {route.params.value}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   Text: {
-    fontSize: '20px',
+    marginTop: 10,
+    fontSize: 20,
   },
 })
