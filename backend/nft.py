e3a613cb61682fc1a42d576e29ee4338fd385d24
Vivacia <varnikabudati@gmail.com>
2021-10-23 13:56:03 -0400

    nft image generation + accessories

--- backend/nft.py
+++ backend/nft.py
@@ -4,32 +4,35 @@ import base64
 import json
 import requests
 from base64 import b64encode
+import math
 
-def generate_index(amount, num_hats, num_glasses, limit):
+def generate_index(amount, limit):
     hat = random.randint(1, int(amount))
     glasses = random.randint(1, int(amount))
-    hat_limit = limit/num_hats
-    glasses_limit = limit/num_glasses
+    hat_limit = limit/4
+    glasses_limit = limit/4
     hat_index = hat_limit/hat
-    if (hat_index >= num_hats):
-        hat_index = num_hats - 1
+    if (hat_index >= 4):
+        hat_index = 4 - 1
     glasses_index = glasses_limit/glasses
-    if (glasses_index >= num_glasses):
-        glasses_index = num_glasses - 1
+    if (glasses_index >= 4):
+        glasses_index = 4 - 1
     return (hat_index, glasses_index)
 
 def generate_nft(amount, filename):
-    hat_index, glasses_index = generate_index(amount, num_hats, num_glasses, limit)
+    hat_index, glasses_index = generate_index(amount, 5)
 
     # combine the hat and the glasses into one image
-    background = Image.open("default_images/hat" + str(hat_index) + ".jpg")
-    foreground = Image.open("default_images/glasses" + str(glasses_index) + ".jpg")
-    Image.alpha_composite(background, foreground).save("generated_images/composite.jpg")
+    background = Image.open("default_images/hat" + str(math.floor(hat_index)) + ".png").resize((2048, 2048))
+    foreground = Image.open("default_images/glasses" + str(math.floor(glasses_index)) + ".png").resize((2048, 2048))
+    result = Image.alpha_composite(background, foreground)
+    result.save("generated_images/composite.png")
 
     # combine shiba and composite image
-    background = Image.open("default_images/shiba.jpg")
-    foreground = Image.open("generated_images/composite.jpg")
-    Image.alpha_composite(background, foreground).save("generated_images/final.jpg")
+    background = Image.open("default_images/shiba.png").resize((2048, 2048)).convert('RGBA')
+    foreground = Image.open("generated_images/composite.png").resize((2048, 2048)).convert('RGBA')
+    result = Image.alpha_composite(background, foreground)
+    result.save("generated_images/final.png")
 
     # upload the image to imgur
     client_id = '678b119f0fd6be0'
@@ -41,10 +44,13 @@ def generate_nft(amount, filename):
         headers = headers,
         data = {
             'key': api_key, 
-            'image': b64encode(open("generated_images/final.jpg", 'rb').read()),
+            'image': b64encode(open("generated_images/final.png", 'rb').read()),
             'type': 'base64',
             'name': filename,
             'title': filename
         }
     )
     return (r.json()['data']['link'])
+
+# if __name__ == '__main__':
+#     generate_nft(300, "test_1_varn")
