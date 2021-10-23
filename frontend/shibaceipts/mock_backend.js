46c25bfac81358a14020fefb42a6c7f3fae5ae7d
Twinov <hoeltgemarc@gmail.com>
2021-10-23 01:39:57 -0400

    update discord cdn hosted images

--- frontend/shibaceipts/mock_backend.js
+++ frontend/shibaceipts/mock_backend.js
@@ -2,22 +2,22 @@ export const MockPosts = [
   {
     user: {
       username: 'DogeEnthusiast',
-      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901301051280490567/dogecoin-elon-musk-snl-memes.png?width=1135&height=631',
+      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
     },
-    shibaceipt: 'https://media.discordapp.net/attachments/892561854503735346/901301051280490567/dogecoin-elon-musk-snl-memes.png?width=1135&height=631',
+    shibaceipt: 'https://media.discordapp.net/attachments/892561854503735346/901340233340174336/funny-shiba-inu-dabbing-prints.png?width=631&height=631',
   },
   {
     user: {
       username: 'ElonMuskFan26',
-      avatar: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.netclipart.com%2Fpp%2Fm%2F123-1231743_doge-head-png-dog-meme-png.png&f=1&nofb=1',
+      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340259093209088/super-fabulous-shiba-prints.png?width=631&height=631',
     },
-    shibaceipt: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.netclipart.com%2Fpp%2Fm%2F123-1231743_doge-head-png-dog-meme-png.png&f=1&nofb=1',
+    shibaceipt: 'https://media.discordapp.net/attachments/892561854503735346/901340259093209088/super-fabulous-shiba-prints.png?width=631&height=631',
   },
   {
     user: {
       username: 'ShibaceiptsToTheMoon',
-      avatar: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.netclipart.com%2Fpp%2Fm%2F123-1231743_doge-head-png-dog-meme-png.png&f=1&nofb=1',
+      avatar: 'https://media.discordapp.net/attachments/892561854503735346/901340319084314644/Capture-5.png?width=665&height=631',
     },
-    shibaceipt: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.netclipart.com%2Fpp%2Fm%2F123-1231743_doge-head-png-dog-meme-png.png',
+    shibaceipt: 'https://media.discordapp.net/attachments/892561854503735346/901340319084314644/Capture-5.png?width=665&height=631',
   },
 ]
