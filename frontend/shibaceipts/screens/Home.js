801fbd544ef3c34406079329b4c8968e472ef523
Twinov <hoeltgemarc@gmail.com>
2021-10-23 03:07:16 -0400

    broken fab

--- frontend/shibaceipts/screens/Home.js
+++ frontend/shibaceipts/screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FlatList, View } from 'react-native'
-import { Card, Button, Icon, Text, Image } from 'react-native-elements'
+import { FlatList, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Card, Button, Icon, Text, Image, FAB } from 'react-native-elements'
 import { MockPosts } from '../mock_backend'
 
 const renderItem = (item) => {
@@ -14,7 +14,7 @@ const renderItem = (item) => {
           </View>
         </Card.Title>
         <View style={{ alignItems: 'center' }}>
-        <Card.Image source={{ uri: item.shibaceipt }} style={{ width: 300, height: 300 }}></Card.Image>
+          <Card.Image source={{ uri: item.shibaceipt }} style={{ width: 300, height: 300 }}></Card.Image>
         </View>
       </View>
     </Card>
@@ -22,5 +22,27 @@ const renderItem = (item) => {
 }
 
 export default function Home() {
-  return <FlatList data={MockPosts} keyExtractor={(item) => item.shibaceipt} renderItem={({ item }) => renderItem(item)} />
+  return (
+    <View>
+      <TouchableOpacity
+        style={{
+          borderWidth: 1,
+          zIndex: 10,
+          borderColor: 'rgba(0,0,0,0.2)',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 70,
+          position: 'absolute',
+          bottom: 10,
+          right: 10,
+          height: 70,
+          backgroundColor: '#fff',
+          borderRadius: 100,
+        }}
+      >
+        <Icon name='photo_camera' type='material' size={30} color='#01a699' />
+      </TouchableOpacity>
+      <FlatList data={MockPosts} keyExtractor={(item) => item.shibaceipt} renderItem={({ item }) => renderItem(item)} />
+    </View>
+  )
 }
