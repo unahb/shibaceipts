3240e8d8737d69661bfa6b84cfacea369e18e6d4
Bhanu Garg <garg.bhanu1@gmail.com>
2021-10-23 13:20:08 -0400

    oof

--- backend/nft.py
+++ backend/nft.py
@@ -5,6 +5,7 @@ import json
 import requests
 from base64 import b64encode
 
+
 def generate_index(amount, num_hats, num_glasses, limit):
     hat = random.randint(1, int(amount))
     glasses = random.randint(1, int(amount))
@@ -18,18 +19,23 @@ def generate_index(amount, num_hats, num_glasses, limit):
         glasses_index = num_glasses - 1
     return (hat_index, glasses_index)
 
-def generate_nft(amount, filename):
-    hat_index, glasses_index = generate_index(amount, num_hats, num_glasses, limit)
+
+def generate_nft(amount, filename, num_hats, num_glasses, limit):
+    hat_index, glasses_index = generate_index(
+        amount, num_hats, num_glasses, limit)
 
     # combine the hat and the glasses into one image
     background = Image.open("default_images/hat" + str(hat_index) + ".jpg")
-    foreground = Image.open("default_images/glasses" + str(glasses_index) + ".jpg")
-    Image.alpha_composite(background, foreground).save("generated_images/composite.jpg")
+    foreground = Image.open("default_images/glasses" +
+                            str(glasses_index) + ".jpg")
+    Image.alpha_composite(background, foreground).save(
+        "generated_images/composite.jpg")
 
     # combine shiba and composite image
     background = Image.open("default_images/shiba.jpg")
     foreground = Image.open("generated_images/composite.jpg")
-    Image.alpha_composite(background, foreground).save("generated_images/final.jpg")
+    Image.alpha_composite(background, foreground).save(
+        "generated_images/final.jpg")
 
     # upload the image to imgur
     client_id = '678b119f0fd6be0'
@@ -37,10 +43,10 @@ def generate_nft(amount, filename):
     api_key = 'bbe58733b5b752b0bca73eedb727d265e13f16c8'
     url = "https://api.imgur.com/3/upload.json"
     r = requests.post(
-        url, 
-        headers = headers,
-        data = {
-            'key': api_key, 
+        url,
+        headers=headers,
+        data={
+            'key': api_key,
             'image': b64encode(open("generated_images/final.jpg", 'rb').read()),
             'type': 'base64',
             'name': filename,
