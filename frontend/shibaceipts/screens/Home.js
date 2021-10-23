59c1f2a2b2e16f6710d9152e7a754fd3e6f8dc4f
Twinov <hoeltgemarc@gmail.com>
2021-10-23 02:37:21 -0400

    to the moon

--- frontend/shibaceipts/screens/Home.js
+++ frontend/shibaceipts/screens/Home.js
@@ -6,14 +6,16 @@ import { MockPosts } from '../mock_backend'
 const renderItem = (item) => {
   return (
     <Card>
-      <View style={{ alignItems: 'center' }}>
-        <Card.Title>
+      <View>
+        <Card.Title style={{ textAlign: 'left' }}>
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
             <Image resizeMode='cover' source={{ uri: item.user.avatar }} style={{ width: 50, height: 50, borderRadius: '50%', marginRight: '15px' }} />
             <Text>{item.user.username}</Text>{' '}
           </View>
         </Card.Title>
-        <Card.Image source={{ uri: item.shibaceipt }} style={{ width: 200, height: 200 }}></Card.Image>
+        <View style={{ alignItems: 'center' }}>
+        <Card.Image source={{ uri: item.shibaceipt }} style={{ width: 300, height: 300 }}></Card.Image>
+        </View>
       </View>
     </Card>
   )
